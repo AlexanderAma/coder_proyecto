15a65210f934bdc8af524a2ede4d4375a857aada
AlexanderAma <121409196+AlexanderAma@users.noreply.github.com>
2023-01-14 17:29:57 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,31 @@
-from django.urls import path
-from . import views
+"""blog_critica URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from critica_cine.views import saludar
+
+
+
+
 
 urlpatterns = [
-    path("", views.saludar, name=""),
-    path("peliculas/", views.listar_peliculas, name="peliculas"),
-    path("series/", views.listar_series, name="series"),
-    path("estrenos/", views.listar_estrenos, name="estrenos"),
-    path("about/", views.about, name="about"),
-    path("crear-serie/", views.crear_serie, name="crear_serie"),
-    path("crear-pelicula/", views.crear_pelicula, name="crear_pelicula"),
-    path("crear-estreno/", views.crear_estreno, name="crear_estreno"),
-    path("buscar-pelicula/", views.buscar_pelicula, name="buscar_pelicula"),
+    path('admin/', admin.site.urls),
+    path("", saludar),
+    path("plataforma/", include("critica_cine.urls"))
+    
+
+    
 ]
